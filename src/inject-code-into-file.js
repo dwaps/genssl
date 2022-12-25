61a5f2d6cec1093146765b1b2ae4af7e50307215
Michael CORNILLON <ei.dwaps@gmail.com>
2022-12-25 11:15:10 +0100

    mkdir recursive

--- src/inject-code-into-file.js
+++ src/inject-code-into-file.js
@@ -6,24 +6,24 @@ const {
 } = require('fs');
 const scriptGentls = require('./script-to-include');
 const debugGentls = require('util').debuglog('gentls');
-const { dirname } = require('path');
+const { dirname, sep, resolve } = require('path');
 
 let { outputFilepath, hasOutputFilepath } = require('./get-args');
 
 if (hasOutputFilepath && existsSync(outputFilepath)) {
-  debugGentls("File exists");
+  debugGentls("File exists: it's ok");
   const content = readFileSync(outputFilepath, { encoding: 'utf-8' });
   writeIntoFile(scriptGentls+content);
 }
 else if (hasOutputFilepath) {
-  debugGentls("File not exists");
+  debugGentls("File not exists --> creating...");
   const folderpath = dirname(outputFilepath);
   try {
-    mkdirSync(folderpath);
-  }
-  catch (e) {}
-  finally {
+    mkdirSync(folderpath, { recursive: true });
     writeIntoFile(scriptGentls);
+  }
+  catch (e) {
+    console.error(e);
   }
 }
 
